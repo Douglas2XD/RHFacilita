da6ff2f4b6a1f61beab94eca6d3fe019db0970e8
Douglas Carneiro <jociandouglas12@gmail.com>
2024-11-16 00:07:40 -0300

    adição de estilização

--- scripts.js
+++ scripts.js
@@ -1,3 +1,23 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const sidebar = document.getElementById("sidebar");
+    const toggleIcon = document.getElementById("toggle-icon");
+    const mainContent = document.getElementById("main-content");
+    
+    sidebar.classList.toggle("hidden");
+    mainContent.classList.toggle("shifted");
+
+    
+    if (sidebar.classList.contains("hidden")) {
+      toggleIcon.classList.remove("fa-times");
+      toggleIcon.classList.add("fa-bars");
+    } else { 
+      toggleIcon.classList.remove("fa-bars");
+      toggleIcon.classList.add("fa-times");
+    }
+
+});
+
 function toggleSidebar() {
     const sidebar = document.getElementById("sidebar");
     const toggleIcon = document.getElementById("toggle-icon");
@@ -5,8 +25,8 @@ function toggleSidebar() {
     
     sidebar.classList.toggle("hidden");
     mainContent.classList.toggle("shifted");
+
     
-    // Alterna o ícone entre hambúrguer e 'X'
     if (sidebar.classList.contains("hidden")) {
       toggleIcon.classList.remove("fa-times");
       toggleIcon.classList.add("fa-bars");
